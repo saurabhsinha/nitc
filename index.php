8dcdd306c3df421d90711737999b507a92b840d1
saurabh kumar <saurabh.nitc10@gmail.com>
2012-02-14 18:24:36 +0530

    first commit with floating footer

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<script type="text/javascript" src="js/expstickybar.js"></script>
+</head>
+<body>
+	<div id="header">
+	</div>
+	<div id="stickybar" class="expstickybar">
+		<div id="sticky_header">
+			<div class="sticky_wrap">
+				<div class="button">
+					<div id="arrow" onclick="javascript:mystickybar.toggle()"></div>
+					<div id="divider"></div>
+					<h4 onclick="javascript:mystickybar.toggle()">
+					<span class="line">Information For</span>
+					Students, Faculty, Alumni & others</h4>
+				</div>
+			</div>
+		</div>
+		<div id="fullline">
+		</div>
+		<div class="sticky_content">
+		</div>
+		<a href="#togglebar"><img/></a>
+	</div>
+</body>
+</html>
