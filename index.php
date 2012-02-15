c47f4d67352d6ec5e9307d09327b0376690b9a0e
saurabh kumar <saurabh.nitc10@gmail.com>
2012-02-15 19:52:32 +0530

    first full skeleton

--- index.php
+++ index.php
@@ -7,26 +7,76 @@
 	<link rel="stylesheet" href="css/style.css" type="text/css">
 	<script type="text/javascript" src="js/expstickybar.js"></script>
 </head>
-<body>
-	<div id="header">
-	</div>
-	<div id="stickybar" class="expstickybar">
-		<div id="sticky_header">
-			<div class="sticky_wrap">
-				<div class="button">
-					<div id="arrow" onclick="javascript:mystickybar.toggle()"></div>
-					<div id="divider"></div>
-					<h4 onclick="javascript:mystickybar.toggle()">
-					<span class="line">Information For</span>
-					Students, Faculty, Alumni & others</h4>
+<body class="full">
+	<div class="body">
+		<div class="wrapper">
+			<div id="header">
+				<div id="wrap-nav">
+					<div class="nav">
+						<ul>
+							<li>
+								<a class="">HOME</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="wrap">
+					<p>
+						<strong class="logo">
+							<a href="http://www.nitc.ac.in"></a>
+						</strong>
+					</p>
+					<form id="search" method="get" action="">
+						<p>
+							<input type="text" placeholder="Search" maxlength="256" id="searchtext" name="searchtext">
+							<input type="image" alt="Search" src="images/btn-search.gif" class="input-search">
+						</p>
+					</form>
+					<div id="utility-nav">
+					</div>
+					<ul id="action-nav">
+						
+					</ul>
 				</div>
 			</div>
+			<div id="content">
+				<div class="wrap">
+					<div id="sub-wrap">
+						<div id="sub">
+						</div>
+					</div>
+					<div id="main-wrap">
+						<div id="main">
+							<div id="main-topic">
+								<div id="main-topic-sub">
+									<div id="slid-show">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+			<div id="stickybar" class="expstickybar">
+				<div id="sticky_header">
+					<div class="sticky_wrap">
+						<div class="button">
+							<div id="arrow" onclick="javascript:mystickybar.toggle()"></div>
+							<div id="divider"></div>
+							<h4 onclick="javascript:mystickybar.toggle()">
+							<span class="line">Information For</span>
+							Students, Faculty, Alumni & others</h4>
+						</div>
+					</div>
+				</div>
+				<div id="fullline">
+				</div>
+				<div class="sticky_content">
+				</div>
+				<a href="#togglebar"><img/></a>
+			</div>
 		</div>
-		<div id="fullline">
-		</div>
-		<div class="sticky_content">
-		</div>
-		<a href="#togglebar"><img/></a>
 	</div>
 </body>
 </html>
